c3231f4b043fe054442139f621433d5466e65b45
Tatsuya Nakano <tatsuya.howdy@gmail.com>
2017-05-01 14:36:44 +0900

    Add visual-studio-code

--- brew.sh
+++ brew.sh
@@ -5,6 +5,7 @@ brew tap caskroom/versions
 brew install tree
 brew install nodebrew
 
+brew cask install visual-studio-code
 brew cask install slack
 brew cask install sourcetree
 brew cask install google-chrome-canary
