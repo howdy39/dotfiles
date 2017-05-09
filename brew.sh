53da4b95d6f0c1d3afd67a0c80895b90136cf745
howdy39 <tatsuya.nakano@topgate.co.jp>
2017-05-09 15:19:00 +0900

    Add some application

--- brew.sh
+++ brew.sh
@@ -10,11 +10,7 @@ brew install nodebrew
 brew install antigen
 brew install phantomjs
 
-brew cask install google-japanese-ime
-brew cask install bettertouchtool
-brew cask install skitch
 brew cask install visual-studio-code
-brew cask install intellij-idea
 brew cask install slack
 brew cask install sourcetree
 brew cask install java7
