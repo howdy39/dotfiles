c92ea15a9c1f5f4f3e8418bd35aa0d2908c4ceff
Tatsuya Nakano <tatsuya.howdy@gmail.com>
2021-07-31 22:43:30 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -11,29 +11,30 @@ brew install wget
 brew install httpie
 brew install jq
 brew install the_silver_searcher
-brew install knqyf263/pet/pet
+# brew install knqyf263/pet/pet
 # brew install antigen
 # brew install goenv
 # brew install rbenv ruby-build
 # brew install ghkw
 brew install direnv
-brew install fish
+# brew install fish
 
-brew install docker
-brew cask install docker
+# brew install docker
+# brew cask install docker
 
 brew cask install visual-studio-code
 brew cask install slack
 # brew cask install sourcetree
 # brew cask install java7
 # brew cask install google-chrome-canary
-brew cask install clipy
+# brew cask install clipy
 # brew cask install mysqlworkbench
 brew cask install iterm2
 # brew cask install google-japanese-ime
 brew cask install skitch
 brew cask install cmd-eikana
 brew cask install bettertouchtool
+brew cask install alfred
 
 # Maven 要Java
 # brew install maven
